496cd49c5246c420b9052e2b7c33b19360ce8eac
apoorvagupta-12 <apoorvagupta.1208@gmail.com>
2025-08-17 16:03:59 -0500

    run 2 at actions

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+"""
+app.py
+
+Script to write a timestamped message to a file and say hi.
+"""
+
 import os
 from datetime import datetime
 
-# Please customize the following variables 
+# Please customize the following variables
 USER_NAME = "apoorvagupta"
 VERSION = "1.0.0"
 
 def say_hi(msg:str = "Hi!", file_directory:str = "/app/data/") -> None:
+    """Timestamped say hi file"""
     # Generate timestamp
     timestamp = datetime.now().strftime("%Y%m%d%H%M")
 
@@ -16,12 +23,13 @@ def say_hi(msg:str = "Hi!", file_directory:str = "/app/data/") -> None:
     file_path = os.path.join(file_directory, file_name)
 
     # Write the timestamp inside the file
-    with open(file_path, "w") as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         file.write(msg)
 
     print(f"File '{file_path}' created successfully.")
 
 def add_numbers(a:int, b:int) -> int:
+    """Return the sum of two numbers."""
     return a + b
 
 if __name__ == "__main__":
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,12 @@
+"""
+test_app.py
+
+Script to test app
+"""
+
 # Run the tests with: pytest tests/test_app.py
 from app import add_numbers
 
 def test_add_numbers():
+    """ Unit test 1 """
     assert 3 == add_numbers(1,2)
